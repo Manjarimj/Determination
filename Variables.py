b75a6190b3a55a23e8222be35074bcf164d3c6fc
mullairajan <mullairajan2000@gmail.com>
2021-10-25 20:53:43 +0530

    Determination 1.0.0

---
+++ Variables.py
@@ -0,0 +1,9 @@
+string = "hello world"
+int = 12
+float = 12.34
+char = 'w'
+boolean = True
+
+print(string)
+
+
---
+++ string_functions.py
@@ -0,0 +1,30 @@
+a = "string functions"
+ans = a.index('g')
+print(ans)
+
+ans = a.capitalize()
+print(ans)
+
+a = '12'
+ans = a.isalpha()
+print(ans)
+
+ans = a.isnumeric()
+print(ans)
+
+a = "a"
+ans = a.isupper()
+print(ans)
+
+ans = a.islower()
+print(ans)
+
+a = "Hello World"
+ans = a.replace("Hello", "HI")
+print(ans)
+
+a = "hello world"
+ans = a.upper()
+print(ans)
+
+
