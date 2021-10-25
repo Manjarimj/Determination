a05f2e66d42a396a3f31fa26f11f6c643caf3c30
mullairajan <mullairajan2000@gmail.com>
2021-10-25 22:13:14 +0530

    determination 1.0.2

---
+++ functions.py
@@ -0,0 +1,15 @@
+def userdefined_isupper(a):
+    ans = ord(a)
+    if 65 <= ans <= 90:
+        return True
+    else:
+        return False
+
+
+b = "r"
+d = "W"
+ans = userdefined_isupper(b)
+ans1 = userdefined_isupper(d)
+print(ans)
+print(ans1)
+
